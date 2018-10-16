9efa980e8be1b3c5da75afb4006ac5ea31e2ac92
antoniy.zyumbyulev <antonious7@gmail.com>
2018-10-16 10:01:15 +0100

    configuring the app module and adding the store logger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,17 @@ import { AppRoutingModule } from './app.routing.module';
 import { WeatherModule } from './weather/weather.module';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule, ActionReducer } from '@ngrx/store';
+import { IState, reducers } from './weather/store';
+import { environment } from '../environments/environment';
+import { storeLogger } from 'ngrx-store-logger';
 
+export function logger(reducer: ActionReducer<IState>): any {
+  return storeLogger()(reducer);
+}
+
+export const metaReducers = environment.production ? [] : [logger];
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +22,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WeatherModule
+    WeatherModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, {metaReducers})
   ],
   providers: [],
   bootstrap: [AppComponent]
