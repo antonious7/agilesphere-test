48d10bc617bf462cd57b81e3bc33abc08acc51a5
antoniy.zyumbyulev <antonious7@gmail.com>
2018-10-16 10:01:36 +0100

    dispatching action from the weather container

--- src/app/weather/weather.container.ts
+++ src/app/weather/weather.container.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { IState } from './store/index';
+import { SearchCitiesAction } from './store/actions/weather';
+
 @Component({
   selector: 'app-weather',
   template: `
   <app-search></app-search>
   <app-results></app-results>  `
 })
+// tslint:disable-next-line:component-class-suffix
 export class WeatherContainer {
 
-  constructor() {}
+  constructor(private store: Store<IState>) {}
 
-  citySearch() {
-    // TO BE IMPLMENTED
+  citySearch(city: string) {
+    this.store.dispatch(new SearchCitiesAction(city));
   }
+
 }
