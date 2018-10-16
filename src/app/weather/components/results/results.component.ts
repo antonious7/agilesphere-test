118fd303bccac25031b06eb12475c4facd3c58b5
antoniy.zyumbyulev <antonious7@gmail.com>
2018-10-16 10:52:05 +0100

    update results component

--- src/app/weather/components/results/results.component.ts
+++ src/app/weather/components/results/results.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
+import { City } from '../../../model/weather';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html'
 })
-export class ResultsComponent implements OnChanges {
-  constructor() { }
-
-  ngOnChanges() {
-    // IMPLEMENT ANYTHING YOU BEKIEVE YOU MIGHT NEED HERE
-  }
+export class ResultsComponent {
+  @Input() cities: Array<City>;
 }
 
 
