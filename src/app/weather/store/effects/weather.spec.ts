5b3df3879f8f5df3feba47392159f0a20b184731
antoniy.zyumbyulev <antonious7@gmail.com>
2018-10-16 10:51:52 +0100

    adding Weather effects

--- src/app/weather/store/effects/weather.spec.ts
+++ src/app/weather/store/effects/weather.spec.ts
@@ -1,1 +1,60 @@
-// TO BE IMPLEMENTED IF YOU DECIDE TO USE NG-RX
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { of } from 'rxjs/observable/of';
+import { hot, cold } from 'jasmine-marbles';
+
+import { SearchCitiesAction, WeatherActionType, SearchCitiesActionSuccess, SearchCitiesActionError } from '../actions/weather';
+import { WeatherService } from '../../weather.service';
+import { WeatherEffects } from './weather';
+
+describe('WeatherEffects', () => {
+  let weatherEffects: WeatherEffects;
+  let weatherService: jasmine.SpyObj<WeatherService>;
+  let actions: any;
+
+  beforeEach(async(() => TestBed.configureTestingModule({
+      imports: [
+      ],
+      providers: [
+        WeatherEffects,
+        provideMockActions(() => actions),
+        {
+          provide: WeatherService,
+          useValue: jasmine.createSpyObj('WeatherService', ['searchWeatherForCity'])
+        }
+      ]
+    })
+  ));
+
+  beforeEach(() => {
+    weatherService = TestBed.get(WeatherService);
+    weatherEffects = TestBed.get(WeatherEffects);
+  });
+
+  describe('searchCity$', () => {
+
+    it('should fire SearchCitySuccessAction on success', () => {
+      weatherService.searchWeatherForCity.and.returnValue(of({}));
+      const triggerAction: SearchCitiesAction = new SearchCitiesAction('test');
+      const expectedAction: SearchCitiesActionSuccess = new SearchCitiesActionSuccess({});
+
+      actions = hot('--a-', { a: triggerAction });
+      const expected = cold('--b', { b: expectedAction });
+
+      expect(weatherEffects.searchCity$).toBeObservable(expected);
+    });
+
+    it('should fire SearchCityFailureAction on failure', () => {
+      weatherService.searchWeatherForCity.and.returnValue(ErrorObservable.create('test'));
+      const triggerAction: SearchCitiesAction = new SearchCitiesAction('test');
+      const expectedAction: SearchCitiesActionError = new SearchCitiesActionError('error');
+
+      actions = hot('--a-', { a: triggerAction });
+      const expected = cold('--b', { b: expectedAction });
+
+      expect(weatherEffects.searchCity$).toBeObservable(expected);
+    });
+  });
+});
